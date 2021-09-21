8b3d61fe6e026ba9178900f8930cfd1dc114aff0
Rohit Jain <rohitjain19060@gmail.com>
2021-09-21 08:34:47 +0530

    :octocat: Completed and working fine

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+with open("input.txt", "r", encoding="utf-8") as f:
+    d = f.readlines()
+    x = []
+    for i in d:
+        y = i.replace("\n", "").strip()
+        x.insert(0, y)
+    b = sorted(set(x))
+with open("output.txt", "w", encoding="utf-8") as r:
+    for i in b:
+        print(str(i).encode("utf-8").decode("utf-8"), file=r)
