b13b886bbfd8ef90715d0cfdd9f9540cfff387ac
Rohit Jain <rohitjain19060@gmail.com>
2022-10-01 05:43:55 +0530

    Removed Heroku Command and Updated yarn through npm

--- update.py
+++ update.py
@@ -3,12 +3,12 @@ import os
 commands = ["deno upgrade",
             "flutter upgrade --force",
             "git update-git-for-windows",
-            "heroku update",
-            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently",
             "rustup update",
             "scoop update",
             "scoop update *",
-            "yarn set version latest"]
+            "yarn set version latest",
+            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn",
+            ]
 
 for c in commands:
     os.system(c)
