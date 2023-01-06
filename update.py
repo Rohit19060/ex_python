c8a87c61d27a775bf99c1ec65bd4104172fa3924
Rohit Jain <rohitjain19060@gmail.com>
2023-01-06 19:24:51 +0530

    👑PNPM Added in Update

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ commands = ["deno upgrade",
             "scoop update",
             "scoop update *",
             "yarn set version latest",
-            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn",
+            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn pnpm",
             ]
 
 for c in commands:
