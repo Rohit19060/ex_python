4fc83bf32140894b5c4959e036880544622b11d6
Rohit Jain <rohitjain19060@gmail.com>
2023-01-08 16:53:18 +0530

    👑pip Auto Update added

--- update.py
+++ update.py
@@ -8,6 +8,7 @@ commands = ["deno upgrade",
             "scoop update *",
             "yarn set version latest",
             "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn pnpm",
+            "python.exe -m pip install --upgrade pip",
             ]
 
 for c in commands:
