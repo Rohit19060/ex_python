ac9d2362f6ce8d286089669b9907fafdb563d330
Rohit Jain <rohitjain19060@gmail.com>
2023-03-25 22:00:30 +0530

    Added Github Copilot CLi Update

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ commands = ["deno upgrade",
             "scoop update",
             "scoop update *",
             "yarn set version latest",
-            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn pnpm",
+            "npm i -g firebase firebase-tools forever grunt-cli jshint mocha node nodemon npm npm-check-updates react-native-cli typescript vercel sass @angular/cli vue-tsc concurrently yarn pnpm @githubnext/github-copilot-cli",
             "python.exe -m pip install --upgrade pip",
             ]
 
